e6599f632ccf44f276a102f1abc989324ddcc387
ernawaty1228 <ernawaty1228@gmail.com>
2024-05-11 21:31:26 +0700

    error tadi karena salah belum mendaftarkan aplikasi dan memberikan string pada tabel yang berhubungan

--- doa/models.py
+++ doa/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from kategori.models import kategori, PenulisDoa
+from kategori.models import *
 
 class Doa(models.Model):
     judul_doa = models.CharField(max_length=100)
@@ -7,5 +7,5 @@ class Doa(models.Model):
     teks_doa_arab = models.TextField()
     arti_doa = models.TextField()
     tanggal_dibuat = models.DateField(auto_now_add=True)
-    kategori = models.ForeignKey('Kategori', on_delete=models.CASCADE)
-    penulis = models.ForeignKey('PenulisDoa', on_delete=models.CASCADE)
+    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
+    penulis = models.ForeignKey(PenulisDoa, on_delete=models.CASCADE)
