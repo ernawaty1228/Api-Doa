9411f291ede1d3c777c98da24a8364bf9b25f5aa
ernawaty1228 <ernawaty1228@gmail.com>
2024-05-11 20:35:22 +0700

    1 tabel

--- doa/models.py
+++ doa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from kategori.models import kategori, PenulisDoa
 
-# Create your models here.
+class Doa(models.Model):
+    judul_doa = models.CharField(max_length=100)
+    teks_doa_latin = models.TextField()
+    teks_doa_arab = models.TextField()
+    arti_doa = models.TextField()
+    tanggal_dibuat = models.DateField(auto_now_add=True)
+    kategori = models.ForeignKey('Kategori', on_delete=models.CASCADE)
+    penulis = models.ForeignKey('PenulisDoa', on_delete=models.CASCADE)
