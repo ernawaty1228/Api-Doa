331ff132bddcd1d3059addb9e899f9e0f35a7515
ernawaty1228 <ernawaty1228@gmail.com>
2024-05-11 20:34:03 +0700

    2 tabel

--- kategori/models.py
+++ kategori/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Kategori(models.Model):
+    nama_kategori = models.CharField(max_length=100)
+
+class PenulisDoa(models.Model):
+    nama_penulis = models.CharField(max_length=100)
